036934ac0c4f9929b23203c62687246eeffa4cd9
esunlon <acousticsung@gmail.com>
2018-12-18 16:39:49 +0800

    adding scrapy.py “

---
+++ scrapy.py
@@ -0,0 +1,15 @@
+import request
+import re
+class Scrapier:
+
+    def decodeHTML(self, url):
+        page = request.urlopen(url)
+        html = page.read().decode('utf-8') #coverts bytes type to string
+        return html
+
+    def downElement(self, html, regstr):
+        elem_reg = re.compile(regstr)
+        elem_url_list = elem_reg.findall(html)
+        for url in elem_url_list:
+            urllib.request.urlretrieve(url, file_name)
+        return elem_url_list
